657b98ee6f3d5b5e9384b71368456ab77fd95ed2
rafael <rafael.rodriguez@mymoria.de>
2023-02-07 18:10:09 +0100

    modified: fetching data with custom hook

---
+++ src/useFetch.js
@@ -0,0 +1,26 @@
+import {useState, useEffect} from "react";
+
+function useFetch(url) {
+
+    const [data, setData] = useState(null);
+    const [isLoading, setIsLoading] = useState(true);
+    const [errorMessage, setErrorMessage] = useState(null);
+  
+    useEffect(() => {
+      fetch(url)
+        .then(response => response.json())
+        .then(results => {
+          // console.log(results.data.children);
+          setIsLoading(false);
+          setData(results);
+        })
+        .catch(error => {
+          setIsLoading(false);
+          setErrorMessage('There was an error');
+        });
+    }, [url]);
+
+    return {data, isLoading, errorMessage}
+}
+
+export default useFetch
