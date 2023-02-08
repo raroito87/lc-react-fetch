8c9a1470ab546e4a746db98d3be61b7be3c1a76a
rafael <rafael.rodriguez@mymoria.de>
2023-02-08 11:14:36 +0100

    modified: episode 15. fetching data with react query

--- src/Reddit.jsx
+++ src/Reddit.jsx
@@ -1,16 +1,31 @@
-import useFetch from './useFetch';
+import { useQuery } from 'react-query';
 
-export default function Reddit() {const {
+export default function Reddit() {
+//   const {
+//   data: posts,
+//   isLoading,
+//   errorMessage,
+// } = useFetch('https://www.reddit.com/r/aww.json');
+
+const {
   data: posts,
   isLoading,
-  errorMessage,
-} = useFetch('https://www.reddit.com/r/aww.json');
+  isError,
+  error,
+  isSuccess,
+} = useQuery('posts', fetchPosts, {retry: false,});
+
+function fetchPosts() {
+  return fetch('https://www.reddit.com/r/aww.json').then(response =>
+    response.json()
+  );
+}
 
   return (
     <div>
       <h2>Reddit API</h2>
       {isLoading && <div>Loading...</div>}
-      {posts && (
+      {isSuccess && (
         <ul>
           {posts.data.children.map(post => (
             <li key={post.data.id}>
@@ -21,7 +36,7 @@ export default function Reddit() {const {
           ))}
         </ul>
       )}
-      {errorMessage && <div>{errorMessage}</div>}
+      {isError && <div>{error.message}</div>}
     </div>
   );
 }
